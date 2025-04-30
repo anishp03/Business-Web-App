295ddb3dcf814794745927a13660e859bbf42e03
Anish Patel <patelanish203@gmail.com>
2025-04-30 03:57:53 -0400

    Update Program.cs

--- TechTickles/Program.cs
+++ TechTickles/Program.cs
@@ -28,7 +28,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
 
 app.Run();
